52a64a166bb55727f0d052df7499532fefdd2a57
alex_023 <12242284@qq.com>
2017-05-25 17:16:49 +0800

    change example
    Modify to supplement the description of the use of "Unsubscribe"

--- example/simple/main.go
+++ example/simple/main.go
@@ -10,8 +10,15 @@ type CountMessage struct {
 	X, Y int
 }
 
+var (
+	eb       = eventbus.New()
+	topic    = "add"
+	clientId = "consumer"
+)
+
 type Consumer struct {
 	testNilMap map[int32]struct{}
+	counter    int
 }
 
 func (c *Consumer) Add(message interface{}) {
@@ -19,20 +26,26 @@ func (c *Consumer) Add(message interface{}) {
 	if !ok {
 		return
 	}
-	//当输入值 %5==0时，故意向nil map 插入值导致崩溃
-	if cMsg.X%5 == 0 {
+	c.counter++
+	//当输入值 %10==0时，故意向nil map 插入值导致崩溃
+	if cMsg.X%10 == 0 {
 		c.testNilMap[2] = struct{}{}
 	} else {
 		fmt.Printf("result:=%v + %v = %v \n", cMsg.X, cMsg.Y, cMsg.X+cMsg.Y)
 
 	}
+
+	if c.counter == 99 {
+		eb.Unsubscribe(topic, clientId)
+	}
 }
 
-//此代码演示消费者订阅，且在某特殊情况下崩溃，系统对其的处理！
+//此代码演示消费者订阅的基本功能：
+// 1.故障隔离
+// 2.内部取消订阅，这可以用于特殊情况下的状态切换
 func main() {
-	eb := eventbus.New()
 	consumer := &Consumer{}
-	eb.Subscribe("add", "consumer", consumer.Add)
+	eb.Subscribe(topic, clientId, consumer.Add)
 	time.Sleep(time.Second)
 	fmt.Println("topics:", eb.GetTopics())
 	for i := 0; i < 100; i++ {
@@ -40,7 +53,10 @@ func main() {
 	}
 	time.Sleep(time.Second)
 
+	//those messages cannot received by consumer,because it unsubscribe this topic
 	for i := 100; i < 200; i++ {
 		eb.Publish("add", CountMessage{i, i * 2})
 	}
+
+	eb.StopGracefull()
 }
